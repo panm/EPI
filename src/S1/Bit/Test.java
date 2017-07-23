39c043ea4c405b568c455fc9b36ab4f1671e6911
Sushil <maravee6@gmail.com>
2017-07-23 12:21:26 -0700

    Testing the Git commit.

--- src/S1/Bit/Test.java
+++ src/S1/Bit/Test.java
@@ -2,7 +2,7 @@ package S1.Bit;
 
 public class Test {
     public static void main(String[] args){
-        String test = "hello there";
+        String test = "hello World";
         String test1 = test;
     }
 }
